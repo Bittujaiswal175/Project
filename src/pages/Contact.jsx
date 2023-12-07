2bf3c7a0d01b76ec2c30698ec9a5a2078d143043
bittu jaiswal <bittujaiswal175@gmail.com>
2023-12-07 22:51:45 +0530

    Commited

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const Contact = () => {
+  return (
+    <div className='grid place-items-center text-richblack-100 text-3xl h-full'>Contact</div>
+  )
+}
+
+export default Contact;
